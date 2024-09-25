4b3d9a97ff6c2ae275529f93fdcfd3bca9e58417
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-09-25 11:33:45 +0300

    chore: init api gateway

---
+++ src/main/java/com/software/modsen/apigateway/config/GatewayConfig.java
@@ -0,0 +1,23 @@
+package com.software.modsen.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("passengerService", p -> p.path("/api/passenger/**")
+                        .uri("lb://passenger-microservice"))
+                .route("driverService", d -> d.path("/api/driver/**")
+                        .uri("lb://driver-microservice"))
+                .route("ridesService", r -> r.path("/api/rides/**")
+                        .uri("lb://rides-microservice"))
+                .route("ratingService", r -> r.path("/api/rating/**")
+                        .uri("lb://rating-microservice"))
+                .build();
+    }
+}
